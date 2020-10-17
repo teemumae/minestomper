42ed813630761aba74690a6f808a293b60f78377
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-17 22:43:37 +0300

    Funktion pohja ja pääohjelma

--- minestomper.py
+++ minestomper.py
@@ -1,1 +1,13 @@
-
+def count_ninjas(x, y, p_room):
+    """
+    Counts the ninjas surrounding one tile in the given room and
+    returns the result. The function assumes the selected tile does
+    not have a ninja in it - if it does, it counts that one as well.
+    """
+room = [['N', ' ', ' ', ' ', ' '],
+        ['N', 'N', 'N', 'N', ' '],
+        ['N', ' ', 'N', ' ', ' '],
+        ['N', 'N', 'N', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ']]
+ninjas = count_ninjas(1,0,room)
