c9da8d15bec8faadf835134cd5ebbe6e3b7d1573
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-18 11:25:29 +0300

    Count_ninjas koodia

--- minestomper.py
+++ minestomper.py
@@ -4,6 +4,14 @@ def count_ninjas(x, y, p_room):
     returns the result. The function assumes the selected tile does
     not have a ninja in it - if it does, it counts that one as well.
     """
+    
+    ninjas = 0
+    if room[y][x] == "N":
+        ninjas += 1
+    for y1 in range(len(p_room)):
+        for x1 in range(len(p_room[y1])):
+            if p_room[y1][x1] == "N" and y1 == y-1:
+                nininjas += 1
 room = [['N', ' ', ' ', ' ', ' '],
         ['N', 'N', 'N', 'N', ' '],
         ['N', ' ', 'N', ' ', ' '],
