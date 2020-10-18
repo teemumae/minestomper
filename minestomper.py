35697047e3acdc3fdb8104463ec30d934aacdd56
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-18 11:48:18 +0300

    Valmis

--- minestomper.py
+++ minestomper.py
@@ -10,12 +10,28 @@ def count_ninjas(x, y, p_room):
         ninjas += 1
     for y1 in range(len(p_room)):
         for x1 in range(len(p_room[y1])):
-            if p_room[y1][x1] == "N" and y1 == y-1:
-                nininjas += 1
+            if p_room[y1][x1] == "N" and y1 == y-1 and x1 == x:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y-1 and x1 == x-1:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y-1 and x1 == x+1:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y and x1 == x-1:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y and x1 == x+1:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y+1 and x1 == x:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y+1 and x1 == x-1:
+                ninjas += 1
+            elif p_room[y1][x1] == "N" and y1 == y+1 and x1 == x+1:
+                ninjas += 1
+    return ninjas
 room = [['N', ' ', ' ', ' ', ' '],
         ['N', 'N', 'N', 'N', ' '],
         ['N', ' ', 'N', ' ', ' '],
         ['N', 'N', 'N', ' ', ' '],
         [' ', ' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' ', ' ']]
-ninjas = count_ninjas(1,0,room)
+ninjas = count_ninjas(1,2,room)
+print(ninjas)
